a6c7550293d1191f70ece82a397d9c620f7ce811
kitamoo <ayaka20111005@yahoo.co.jp>
2026-01-05 07:39:23 +0900

    OpenWeatherServiceにHttpClientを受け取るコンストラクタを追加

--- Services/OpenWeatherService.cs
+++ Services/OpenWeatherService.cs
@@ -1,9 +1,17 @@
-﻿using TouringChecker.Dtos;
+﻿using System.Net.Http;
+using TouringChecker.Dtos;
+
 
 namespace TouringChecker.Services
 {
     public class OpenWeatherService
     {
+        private readonly HttpClient _httpClient;
+
+        public OpenWeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         public async Task<OpenWeatherResponse> GetForecastAsync(string city)
         {
             throw new NotImplementedException();
